f3b1fb3c8290cfe87109557f0d356f59b291e4a3
Knowblesse <knowblesse@gmail.com>
2025-10-28 18:43:21 +0900

    Removed Oxygen & Hello message + changed to u8g2

--- AirAir/AirAir.ino
+++ AirAir/AirAir.ino
@@ -1,10 +1,7 @@
-#include "LOX02.h"
 #include "SHT1X.h"
 #include "TES0902.h"
 
 #define PIN_FAN 2
-#define PIN_O2TX 3
-#define PIN_O2RX 4
 #define PIN_BUZZER 5
 #define PIN_CO2TX 7
 #define PIN_CO2RX 14
@@ -13,16 +10,15 @@
 #define PIN_RBUTTON 9
 #define PIN_LBUTTON 10
 
-LOX02 lox(PIN_O2RX, PIN_O2TX);
 SHT1X sht1x(PIN_TEMP_SDA, PIN_TEMP_SCK);
 TES0902 tes(PIN_CO2RX, PIN_CO2TX);
 
 // Screen
-#include <U8x8lib.h>
-#ifdef U8X8_HAVE_HW_SPI
+#include <U8g2lib.h>
 #include <SPI.h>
-#endif
-U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(8);
+#include <Wire.h>
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); // No Reset pin
 
 // ISR
 bool fanState = true;
@@ -30,7 +26,6 @@ void ventilate(){
   fanState = !fanState;
   digitalWrite(PIN_FAN, fanState);
 }
-
 
 // variables for CO2 warnings
 const int CO2_LIMIT = 2000;
@@ -60,14 +55,15 @@ void setup(void)
   pinMode(PIN_RBUTTON, INPUT_PULLUP);
   pinMode(PIN_BUZZER, OUTPUT);
   pinMode(PIN_FAN, OUTPUT);
-  lox.initialize();
+
   sht1x.initialize();
   tes.initialize();
   attachInterrupt(digitalPinToInterrupt(PIN_LBUTTON), ventilate, FALLING);
 
   // Screen
-  u8x8.begin();
-  u8x8.setPowerSave(0);
+  u8g2.begin();
+  u8g2.setPowerSave(0);
+  u8g2.setContrast(255);
 
   Serial.begin(9600);
 
@@ -76,132 +72,65 @@ void setup(void)
 }
 
 void updateScreen(){
+  u8g2.clearBuffer();
   /*************************************/
   /*           Temperature             */
   /*************************************/
-  u8x8.setFont(u8x8_font_7x14B_1x2_f);
-  u8x8.drawString(0,0,"T");
+  u8g2.setFont(u8g2_font_lubR14_tf );
   float temp = sht1x.readTemp();
   if(sht1x.integrityTemp){
-    u8x8.drawString(1,0,String(temp,1).c_str());
-    u8x8.drawGlyph(5,0,176);
-    u8x8.drawString(6,0,"C");
+    u8g2.drawStr(8,17,String(temp,1).c_str());
+    u8g2.drawGlyph(13*4+8,17,176);
+    u8g2.drawStr(13*5+3,17,"C");
   }
   else{
-    u8x8.drawString(1,0," err");
-    u8x8.drawString(5,0,176);
-    u8x8.drawString(6,0,"C");
+    u8g2.drawStr(0,17," err");
+    u8g2.drawGlyph(13*4+8,17,176);
+    u8g2.drawStr(13*5+3,17,"C");
   }
 
   /*************************************/
   /*             Humidity              */
   /*************************************/
-  u8x8.drawString(9,0,"H");
-  float humd = sht1x.readHumd();
+    float humd = sht1x.readHumd();
   if (sht1x.integrityHumd){
-    u8x8.drawString(10,0,(String(humd)+"%").c_str());
+
+    u8g2.drawStr(8,40,(String(humd,1) +"  %").c_str());
+
   }
   else{
-    u8x8.drawString(10,0," err %");
-  }
-
-
-   /*************************************/
-  /*             Pressure              */
-  /*************************************/
-  u8x8.drawString(0,2,"P");
-  double pressure = lox.getP();
-  if (pressure < 0){
-    u8x8.drawString(2,2," err p");
-  }
-  else if (pressure<1000){
-    u8x8.drawString(2,2,(" " + String((int)pressure)+"p").c_str());
-  }
-  else {
-    u8x8.drawString(2,2,(String((int)pressure)+"p").c_str());
-  }
-
-  /*************************************/
-  /*            O2 Percent             */
-  /*************************************/
-  u8x8.drawString(9,2,"O");
-  double O2percent = lox.getO2();
-    if(O2percent > 0){
-    u8x8.drawString(10,2,(String(O2percent)+"%").c_str());
-  }
-  else{
-    u8x8.drawString(10,2," err %");
+    u8g2.drawStr(8,40," err  %");
   }
 
   /*************************************/
   /*              CO2 PPM              */
   /*************************************/
-  u8x8.drawString(0,4,"C");
   int CO2_val = tes.readCO2();
   if (tes.integrityCO2){
     if (CO2_val < 1000){
-      u8x8.drawString(2,4,(" " + String(CO2_val)+"p").c_str());  
+      u8g2.drawStr(8,64,(" " + String(CO2_val)+"  PPM").c_str());  
     }
     else{
-      u8x8.drawString(2,4,(String(CO2_val)+"p").c_str());
+      u8g2.drawStr(8,64,(String(CO2_val)+"  PPM").c_str());
     }
   }
   else{
-    u8x8.drawString(2,4," err p");
+    u8g2.drawStr(8,64," err  PPM");
   }
 
-  /*************************************/
-  /*               O2 PPM              */
-  /*************************************/
-  u8x8.drawString(9,4,"OP");
-  double o2pressure = lox.getO2P();
-  if (o2pressure < 0){
-    u8x8.drawString(1,4," err p");
-  }
-  else if (o2pressure<1000){
-    u8x8.drawString(11,4,(" " + String((int)o2pressure)+"p").c_str());
-  }
-  else {
-    u8x8.drawString(11,4,(String((int)o2pressure)+"p").c_str());
-  }
  
-  /*************************************/
-  /*       Show Rotating Mark          */
-  /*************************************/
-  if(currRotateState == 3){
-    currRotateState = 0;
-  }
-  else{
-    currRotateState++;
-  }
-  u8x8.drawString(14, 6, rotateString[currRotateState].c_str());
+  // /*************************************/
+  // /*       Show Rotating Mark          */
+  // /*************************************/
+  // if(currRotateState == 3){
+  //   currRotateState = 0;
+  // }
+  // else{
+  //   currRotateState++;
+  // }
+  // u8x8.drawString(14, 6, rotateString[currRotateState].c_str());
 
-  /*************************************/
-  /*        Show Hello Message         */
-  /*************************************/
-  if(currHelloMsg < 7){
-    u8x8.drawString(0, 6, helloMsg[currHelloMsg].c_str());
-    currHelloMsg++;
-  }
-  else{
-    if(CO2_val > CO2_LIMIT){
-      u8x8.drawString(0, 6, "  !Ventilate!");
-      if(isBuzzerArmed && screenOn && (millis()-lastWarning > WARNTIME)){
-        for(int j=0; j<3; j++){
-          digitalWrite(PIN_BUZZER, HIGH);
-          delay(200);
-          digitalWrite(PIN_BUZZER, LOW);
-          delay(200);
-        }
-        lastWarning = millis();
-      }
-    }
-    else{
-      u8x8.drawString(0, 6, "             ");
-    }
-  }
-
-  u8x8.refreshDisplay();    // only required for SSD1606/7  
+  u8g2.sendBuffer();
 }
 
 void loop(void)
@@ -215,7 +144,7 @@ void loop(void)
       currHelloMsg = 0;
     }
     isBuzzerArmed = true;
-    u8x8.setPowerSave(!screenOn);
+    u8g2.setPowerSave(!screenOn);
   }
   delay(500);
 }
